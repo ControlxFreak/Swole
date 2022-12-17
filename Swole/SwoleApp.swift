1257a75f898934c27d3bde9a80df242fa5426a5f
Anthony Trezza <anthony.t.trezza@gmail.com>
2022-12-16 19:19:04 -0500

    Initial Commit

---
+++ Swole/SwoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwoleApp.swift
+//  Swole
+//
+//  Created by Anthony Trezza on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SwoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
