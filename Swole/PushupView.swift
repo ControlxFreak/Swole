201b85e77b119dafcb7c6d382ef28c3ac77e03b5
Anthony Trezza <anthony.t.trezza@gmail.com>
2022-12-16 21:03:44 -0500

    Initial commit. Started design.

---
+++ Swole/PushupView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Swole
+//
+//  Created by Anthony Trezza on 12/13/22.
+//
+
+import SwiftUI
+import ConfettiSwiftUI
+
+struct PushupView: View {
+    @State private var manmode: Bool = false
+    @State private var count: Int = 0
+    @State private var goal: Int = 10000
+    @State private var increment: Int = 25
+
+    var body: some View {
+        VStack(spacing: 10) {
+            Spacer()
+            Button {
+                count += increment
+            } label: {
+                Image("cat-pushup")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            }.confettiCannon(counter: $count, num: 50, radius: 500.0)
+            ProgressView(value: Float(count), total: Float(goal))
+            {
+                HStack{
+                    Text("\(count) / \(goal)")
+                    Spacer()
+                    Text(String(format: "%.2f", 100*Float(count)/Float(goal)) + "%")
+                }
+            }.padding()
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct PushupView_Previews: PreviewProvider {
+    static var previews: some View {
+        PushupView()
+    }
+}
--- Swole/SwoleApp.swift
+++ Swole/SwoleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwoleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PushupView()
         }
     }
 }
